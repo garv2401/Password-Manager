c96b8e2e7ff93157f3a357640e37f59c745266ab
GARVIT <118100178+garv2401@users.noreply.github.com>
2024-09-16 11:35:57 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-slate-800 text-white  fixed bottom-0 w-screen text-center' >&copy; 2024 www.PassOP.com. All rights reserved.</div>
+    <div className='bg-slate-800 text-white  fixed bottom-0 w-screen text-center' >&copy; 2024 www.LockVault.com. All rights reserved.</div>
   )
 }
 
